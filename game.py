dd5161af5ea519849d76ac7103ef937900f5c7c9
Aixa Charif <aixajchaif@gmail.com>
2024-03-16 00:49:29 -0300

    Bug string vacío solucionado + mejoras al ingresar letras

--- game.py
+++ game.py
@@ -1,38 +1,47 @@
 import random
+
 # Lista de palabras posibles
 words = ["python", "programación", "computadora", "código", "desarrollo",
 "inteligencia"]
+
 # Elegir una palabra al azar
 secret_word = random.choice(words)
+
 # Número máximo de intentos permitidos
 max_attempts = 10
+
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
+
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
+
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
 while (max_attempts != 0):
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
-
+    
+    #Verificar si la letra es correcta
+    if (len(letter)!= 1) or (not letter.isalpha()):
+        print ("Error, intentalo nuevamente")
+        continue
     # Verificar si la letra ya ha sido adivinada
-    if letter in guessed_letters:
+    elif (letter in guessed_letters):
         print("Ya has intentado con esa letra. Intenta con otra.")
         continue
-    
-    # Agregar la letra a la lista de letras adivinadas
-    guessed_letters.append(letter)
-    
     # Verificar si la letra está en la palabra secreta
-    if (letter in secret_word) and (len(letter) == 1): 
+    elif (letter in secret_word): 
         print("¡Bien hecho! La letra está en la palabra.")
     else:
         print("Lo siento, la letra no está en la palabra.")
         max_attempts = max_attempts - 1 
     
+    # Agregar la letra a la lista de letras adivinadas
+    guessed_letters.append(letter)
+
     # Mostrar la palabra parcialmente adivinada
     letters = []
     for letter in secret_word:
